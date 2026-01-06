4d69c1a4a437ce974b5ebcb392b0e8472a8202d4
myfmynm-hash <myfmynm@gmail.com>
2026-01-06 17:46:29 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,25 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  
+  // Укажите здесь имя вашего репозитория между слэшами !!!!!!!!!!!!!!!!!!!!!
+  base: '/ai-engineer/', 
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    // Оптимизация сборки: объединение мелких CSS файлов в один
+    cssCodeSplit: false,
+    // Убираем предупреждения о размере бандла для тяжелых нейросетевых библиотек
+    chunkSizeWarningLimit: 1000,
+  },
+  resolve: {
+    alias: {
+      // Это позволит использовать красивые пути в будущем (например, '@/components/...')
+      '@': '/',
+    },
+  },
+})
